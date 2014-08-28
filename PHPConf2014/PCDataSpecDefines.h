0e46090fb1a80085fa521aa717eaab2b89d7307d
Vincent Chen <vince78718@gmail.com>
2014-08-28 22:30:36 +0800

    update gitignore

---
+++ PHPConf2014/PCDataSpecDefines.h
@@ -0,0 +1,13 @@
+//
+//  PCDataSpecDefines.h
+//  PHPConf2014
+//
+//  Created by vincent on 2014/08/28.
+//  Copyright (c) 2014年 Vincent Chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PCDataSpecDefines : NSObject
+
+@end
---
+++ PHPConf2014/PCDataSpecDefines.m
@@ -0,0 +1,13 @@
+//
+//  PCDataSpecDefines.m
+//  PHPConf2014
+//
+//  Created by vincent on 2014/08/28.
+//  Copyright (c) 2014年 Vincent Chen. All rights reserved.
+//
+
+#import "PCDataSpecDefines.h"
+
+@implementation PCDataSpecDefines
+
+@end
