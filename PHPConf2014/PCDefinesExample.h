c6fc2afc8d12b9b471b34b9df130c7a0f71ea748
Vincent Chen <vince78718@gmail.com>
2014-08-28 23:07:24 +0800

    add defines template files

---
+++ PHPConf2014/PCDefinesExample.h
@@ -0,0 +1,35 @@
+//
+//  PCDefines.h
+//  PHPConf2014
+//
+//  Created by vincent on 2014/08/28.
+//  Copyright (c) 2014年 Vincent Chen. All rights reserved.
+//
+
+#ifndef PCDEFINES
+#define PCDEFINES
+
+#pragma mark - API
+
+extern NSString *const kPCApplicationID;
+
+#pragma mark - API - Data Spec Secrets
+
+extern NSString *const kPCDSRequestTemplate;
+extern NSString *const kPCDSDataVersion;
+
+extern NSString *const kPCDSSessionR0;
+extern NSString *const kPCDSSessionR2;
+extern NSString *const kPCDSVenueMap;
+
+extern NSString *const kPCDSSponsorGold;
+extern NSString *const kPCDSSponsorSilver;
+extern NSString *const kPCDSSponsorDiamond;
+extern NSString *const kPCDSSponsorSpecialThanks;
+
+extern NSString *const kPCDSConfBulletin;
+extern NSString *const kPCDSConfAbout;
+extern NSString *const kPCDSConfTeam;
+extern NSString *const kPCDSAppTeam;
+
+#endif
---
+++ PHPConf2014/PCDefinesExample.m
@@ -0,0 +1,32 @@
+//
+//  PCDefines.m
+//  PHPConf2014
+//
+//  Created by vincent on 2014/08/28.
+//  Copyright (c) 2014年 Vincent Chen. All rights reserved.
+//
+
+#import "PCDefines.h"
+
+#pragma mark - API
+
+NSString *const kPCApplicationID = <#@"ApplicationID"#>;
+
+#pragma mark - API - Data Spec Secrets
+
+NSString *const kPCRequestTemplate = <#@"secret"#>;
+NSString *const kPCDataVersion = <#@"secret"#>;
+
+NSString *const kPCSessionR0 = <#@"secret"#>;
+NSString *const kPCSessionR2 = <#@"secret"#>;
+NSString *const kPCVenueMap = <#@"secret"#>;
+
+NSString *const kPCSponsorGold = <#@"secret"#>;
+NSString *const kPCSponsorSilver = <#@"secret"#>;
+NSString *const kPCSponsorDiamond = <#@"secret"#>;
+NSString *const kPCSponsorSpecialThanks = <#@"secret"#>;
+
+NSString *const kPCConfBulletin = <#@"secret"#>;
+NSString *const kPCConfAbout = <#@"secret"#>;
+NSString *const kPCConfTeam = <#@"secret"#>;
+NSString *const kPCAppTeam = <#@"secret"#>;
