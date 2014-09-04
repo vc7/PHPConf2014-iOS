e7283faeadd85c2d9dcc33422c4c59f4fbecccc1
Vincent Chen <vince78718@gmail.com>
2014-09-04 22:49:02 +0800

    Apply basic style sheet.

--- PHPConf2014/PCAppDelegate.m
+++ PHPConf2014/PCAppDelegate.m
@@ -10,6 +10,8 @@
 #import "PCDefines.h"
 #import "PCDataSpecDefines.h"
 
+#import "UIColor+PHPConfAdditions.h"
+
 @implementation PCAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -20,10 +22,27 @@
     [PCKit setApplicationId:kPCApplicationID
                    dataSpec:[[PCDataSpecDefines alloc] init]];
     
-    NSLog(@"%@", [PCKit getApplicationId]);
+    [self applyStyleSheet];
     
     [self.window makeKeyAndVisible];
     return YES;
+}
+
+#pragma mark - Style
+
+- (void)applyStyleSheet
+{
+    // Add settings into the plist to make status text white
+    // 1) UIViewControllerBasedStatusBarAppearance -> NO
+    // 2) UIStatusBarStyle -> UIStatusBarStyleBlackOpaque
+    
+    // UINavigationBar's background color
+    [UINavigationBar appearance].barTintColor = [UIColor phpconfBlueColor];
+    [UINavigationBar appearance].barStyle = UIBarStyleBlackTranslucent;
+    [UINavigationBar appearance].tintColor = [UIColor whiteColor];
+    [UINavigationBar appearance].titleTextAttributes = @{ NSForegroundColorAttributeName:[UIColor whiteColor] };
+    
+    [UITabBar appearance].tintColor = [UIColor phpconfBlueColor];
 }
 
 @end
