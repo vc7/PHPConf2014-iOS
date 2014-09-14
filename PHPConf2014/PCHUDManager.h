d6c08c40b0c1c485922a1619b9ea262e048cbe62
Vincent Chen <vince78718@gmail.com>
2014-09-14 15:46:24 +0800

    Finish the basic functionality of the PCHUDManager.

--- PHPConf2014/PCHUDManager.h
+++ PHPConf2014/PCHUDManager.h
@@ -8,8 +8,28 @@
 
 #import <Foundation/Foundation.h>
 
+@class MBProgressHUD;
+
 @interface PCHUDManager : NSObject
 
+@property (nonatomic, strong) UIView *parentView;
+@property (nonatomic, strong, readonly) MBProgressHUD *progressHUD;
+
+/*! The default manager.
+ @discussion The HUD that is managed by the default manager is set to lay onto the main window's root view controller, covered on the whole screen by default.
+ @return the shared instance of default manager.
+ */
 + (instancetype)defaultManager;
+
+/*! Create a custom manager by giving a parent view
+ @param parentView the givin view are going to add HUD on.
+ @return the instance of a custom manager.
+ */
+- (instancetype)initWithParentView:(UIView *)parentView;
+
+/*! The most easy way to pop the managed HUD view out */
+- (void)showHUD;
+/*! The most easy way to dismiss the managed HUD view */
+- (void)hideHUD;
 
 @end
--- PHPConf2014/PCHUDManager.m
+++ PHPConf2014/PCHUDManager.m
@@ -7,20 +7,72 @@
 //
 
 #import "PCHUDManager.h"
+#import "PCAppDelegate.h"
+
+#import <MBProgressHUD/MBProgressHUD.h>
 
 @interface PCHUDManager ()
+
+@property (nonatomic, strong, readwrite) MBProgressHUD *progressHUD;
 
 @end
 
 @implementation PCHUDManager
 
+#pragma mark - Singleton
+
 + (instancetype)defaultManager {
     static dispatch_once_t pred;
     static PCHUDManager *shared = nil;
     dispatch_once(&pred, ^{
-        shared = [[PCHUDManager alloc] init];
+        
+        UIView *parentView = ((PCAppDelegate *)[[UIApplication sharedApplication] delegate]).window.rootViewController.view;
+        
+        shared = [[PCHUDManager alloc] initWithParentView:parentView];
+
     });
     return shared;
+}
+
+#pragma mark - Life Cycle
+
+- (instancetype)initWithParentView:(UIView *)parentView
+{
+    self = [super init];
+    if (self) {
+        self.parentView = parentView;
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [self.progressHUD removeFromSuperview];
+}
+
+#pragma mark - Accessors
+
+- (MBProgressHUD *)progressHUD
+{
+    NSAssert(self.parentView, @"Set a parent view before using progressHUD.");
+    if (!_progressHUD) {
+        _progressHUD = [[MBProgressHUD alloc] initWithView:self.parentView];
+        [self.parentView addSubview:_progressHUD];
+    }
+    
+    return _progressHUD;
+}
+
+#pragma mark - Public Methods
+
+- (void)showHUD
+{
+    [self.progressHUD show:YES];
+}
+
+- (void)hideHUD
+{
+    [self.progressHUD hide:YES];
 }
 
 @end
