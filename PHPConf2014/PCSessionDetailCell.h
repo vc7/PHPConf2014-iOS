124dc1d6b63814817daef466e39bc420d71ba1e1
Vincent Chen <vince78718@gmail.com>
2014-09-14 20:52:45 +0800

    Adding two subclasses for the first section of session detail view.

--- PHPConf2014/PCSessionDetailCell.h
+++ PHPConf2014/PCSessionDetailCell.h
@@ -8,6 +8,31 @@
 
 #import <UIKit/UIKit.h>
 
+/*!
+ @class PCSessionDetailCell
+ The base class that is going to use in the session detail view.
+ */
 @interface PCSessionDetailCell : UITableViewCell
+
+@end
+
+/*!
+ @class PCSessionDetailHeroCell
+ The cell class to display the top banner, speaker's name and position.
+ */
+@interface PCSessionDetailHeroCell : PCSessionDetailCell
+
+@property (nonatomic, strong) UIImageView *avatarImageView;
+@property (nonatomic, strong) NSString *speakerName;
+@property (nonatomic, strong) NSString *speakerPosition;
+@property (nonatomic, strong) NSString *venueName;
+
+@end
+
+/*!
+ @class PCSessionDetailIntroCell
+ The cell class that display the title of the session.
+ */
+@interface PCSessionDetailIntroCell : PCSessionDetailCell
 
 @end
--- PHPConf2014/PCSessionDetailCell.m
+++ PHPConf2014/PCSessionDetailCell.m
@@ -8,12 +8,185 @@
 
 #import "PCSessionDetailCell.h"
 
+#import "UIColor+PHPConfAdditions.h"
+
 @implementation PCSessionDetailCell
 
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        [self applyStyleSheet];
+    }
+    return self;
+}
 
-    // Configure the view for the selected state
+#pragma mark - Private Methods
+
+- (void)applyStyleSheet
+{
+    self.backgroundColor = [UIColor clearColor];
+    self.selectionStyle = UITableViewCellSelectionStyleNone;
+}
+
+@end
+
+@interface PCSessionDetailHeroCell ()
+
+@property (nonatomic, strong) UIView *titleHUD;
+@property (nonatomic, strong) UILabel *nameLabel;
+@property (nonatomic, strong) UILabel *positionLabel;
+
+@end
+
+@implementation PCSessionDetailHeroCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        [self applyStyleSheet];
+    }
+    return self;
+}
+
+- (void)layoutSubviews
+{
+    CGFloat titleHUDHeight = 80;
+    self.titleHUD.frame = (CGRect){{0, CGRectGetHeight(self.bounds) - titleHUDHeight}, {CGRectGetWidth(self.bounds), titleHUDHeight}};
+    
+    CGFloat labelMargin = 20;
+    
+    CGFloat titleLabelHeight = 18;
+    self.nameLabel.frame = (CGRect){{ 25, labelMargin }, {CGRectGetWidth(self.titleHUD.frame) - labelMargin - 70, titleLabelHeight}};
+    
+    CGFloat positionLabelHeight = 16;
+    self.positionLabel.frame = (CGRect){{ 25, CGRectGetHeight(self.titleHUD.frame) - labelMargin - positionLabelHeight}, {CGRectGetWidth(self.titleHUD.frame) - labelMargin - 70, positionLabelHeight}};
+}
+
+#pragma mark - Accessors
+
+- (void)setSpeakerName:(NSString *)speakerName
+{
+    if (_speakerName != speakerName) {
+        
+        if (speakerName && ![speakerName isEqualToString:@""]) {
+            _speakerName = speakerName;
+        } else {
+            _speakerName = @"--";
+        }
+        
+        self.nameLabel.text = _speakerName;
+    }
+}
+
+- (void)setSpeakerPosition:(NSString *)speakerPosition
+{
+    if (_speakerPosition != speakerPosition) {
+        
+        if (speakerPosition && ![speakerPosition isEqualToString:@""]) {
+            _speakerPosition = speakerPosition;
+        } else {
+            _speakerPosition = @"--";
+        }
+        
+        self.positionLabel.text = _speakerPosition;
+    }
+}
+
+- (void)setVenueName:(NSString *)venueName
+{
+    if (_venueName != venueName) {
+        _venueName = venueName;
+    }
+}
+
+- (UIImageView *)avatarImageView
+{
+    if (!_avatarImageView) {
+        _avatarImageView = [[UIImageView alloc] initWithFrame:CGRectZero];
+        _avatarImageView.contentMode = UIViewContentModeScaleAspectFill;
+        [self addSubview:_avatarImageView];
+    }
+    return _avatarImageView;
+}
+
+- (UIView *)titleHUD
+{
+    if (!_titleHUD) {
+        _titleHUD = [[UIView alloc] initWithFrame:CGRectZero];
+        [self addSubview:_titleHUD];
+    }
+    
+    return _titleHUD;
+}
+
+- (UILabel *)nameLabel
+{
+    if (!_nameLabel) {
+        _nameLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+        _nameLabel.text = @"Default Name";
+        [self.titleHUD addSubview:_nameLabel];
+    }
+    
+    return _nameLabel;
+}
+
+- (UILabel *)positionLabel
+{
+    if (!_positionLabel) {
+        _positionLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+        _positionLabel.text = @"Default Position";
+        [self.titleHUD addSubview:_positionLabel];
+    }
+    
+    return _positionLabel;
+}
+
+#pragma mark - Private Methods
+
+- (void)applyStyleSheet
+{
+    [super applyStyleSheet];
+    
+    self.backgroundColor = [UIColor clearColor];
+    
+    self.avatarImageView.backgroundColor = [UIColor grayColor];
+    self.titleHUD.backgroundColor = [UIColor colorWithWhite:0 alpha:0.8];
+    
+    self.nameLabel.backgroundColor = [UIColor clearColor];
+    self.nameLabel.textColor = [UIColor whiteColor];
+    self.nameLabel.font = [UIFont boldSystemFontOfSize:16.f];
+    
+    self.positionLabel.backgroundColor = [UIColor clearColor];
+    self.positionLabel.textColor = [UIColor whiteColor];
+    self.positionLabel.font = [UIFont systemFontOfSize:14.f];
+
+}
+
+@end
+
+@implementation PCSessionDetailIntroCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        [self applyStyleSheet];
+    }
+    return self;
+}
+
+#pragma mark - Private Methods
+
+- (void)applyStyleSheet
+{
+    [super applyStyleSheet];
+    
+    self.textLabel.font = [UIFont systemFontOfSize:14.f];
+    self.textLabel.textColor = [UIColor phpconfBlackColor];
+    self.textLabel.lineBreakMode = NSLineBreakByWordWrapping;
+    self.textLabel.numberOfLines = 0;
 }
 
 @end
